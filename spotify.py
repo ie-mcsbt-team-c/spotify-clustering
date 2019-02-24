df3cf3b51f46b44b1437c6837f0327c5cb5df316
David <dabago@student.ie.edu>
2019-02-25 00:39:25 +0100

    sil + elbow

--- spotify.py
+++ spotify.py
@@ -131,15 +131,202 @@ audio_array_scaled = mms.transform(audio_array)
 #CLUSTERING
 ############
 
-model = KMeans(n_clusters=4).fit(audio_array_scaled)
+#Here I still need to figure out the explanation why k-means would work well
+
+#Our first goal is to find what number of clusters we need. We will use Elbow method and back it up with silohoutte. 
+
+
+#%%
+
+#ELBOW METHOD#
+#If you graph the percentage of variance explained by 
+#the clusters against the number of clusters, the first clusters will 
+#add much information (explain a lot of variance), but at some point 
+#the marginal gain will drop, giving an angle in the graph. 
+#The number of clusters are chosen at this point, hence the “elbow criterion”.
+
+ks = range(1, 10)
+inertias = []
+
+def number_of_clusters():
+    
+    for k in ks:
+        model = KMeans(n_clusters=k, random_state=None)
+        model.fit(audio_array_scaled)
+        inertias.append(model.inertia_)
+        print({"Number of cluster":k})
+        print({"Model Inertia Value":model.inertia_})
+
+
+# We can see there is no a significant different after 5 clusters
+# Inertia refers to within-cluster sum-of-squares of all clusters. Then show how well packed the clusters. 
+# Our goal is to         
+
+
+number_of_clusters()
+
+plt.plot(ks, inertias, '-o')
+plt.xlabel('number of clusters, k')
+plt.ylabel('Percentage of variance explained')
+plt.figtext(.5, .8, "Explanation:")
+plt.figtext(.5, .75, "How much intra-cluster variance")
+plt.figtext(.5, .7, "is explained when we add")
+plt.figtext(.5, .65, "a new cluster")
+plt.xticks(ks)
+plt.show()
+
+#%%
+
+#SILHOUETTE ANALYSIS#
+
+#Silhouette coefficients 
+#(as these values are referred to as) near +1 
+#indicate that the sample is far away from the
+# neighboring clusters. A value of 0 indicates 
+#that the sample is on or very close to the decision 
+#boundary between two neighboring clusters and negative 
+#values indicate that those samples might have been assigned 
+#to the wrong cluster.
+#
+#Avg Silhouette score: I get the average Silhouette score of the cluster k.
+#Silhouette samples score: And, for all the samples belonging to a given cluster 
+#(from 1 to k), I calculate the individual silhouette score of each sample belong 
+#to that cluster.
+
+# Important #
+
+#Negative values means that they are not in the right cluster since they might
+#be closer to other centroids from other clusters. 
+
+#Then I sort the scores of all samples belonging to each cluster. 
+#This is needed so that I can plot the score in an ascending order.
+
+#s(i) = {b(i) - a(i)} / {\max\{a(i),b(i)\}}
+#
+
+
+#Explnation here https://www.youtube.com/watch?v=5TPldC_dC0s
+
+
+range_n_clusters = [4,5,6]
+scores=[]
+
+def number_of_clusters_silhouette():
+        
+    for n_clusters in range_n_clusters:
+        # Create a subplot with 1 row and 2 columns
+        fig, (ax1, ax2) = plt.subplots(1, 2)
+        fig.set_size_inches(18, 7)
+         
+        # The 1st subplot is the silhouette plot
+        # The silhouette coefficient can range from -1, 1 but in this example all
+        # lie within [-0.1, 1]
+        ax1.set_xlim([-0.1, 1])
+        # The (n_clusters+1)*10 is for inserting blank space between silhouette
+        # plots of individual clusters, to demarcate them clearly.
+        ax1.set_ylim([0, len(audio_array_scaled) + (n_clusters + 1) * 10])
+        
+        # Initialize the clusterer with n_clusters value and a random generator
+        # seed of 10 for reproducibility.
+        clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+        cluster_labels = clusterer.fit_predict(audio_array_scaled)
+        
+        # The silhouette_score gives the average value for all the samples.
+        # This gives a perspective into the density and separation of the formed
+        # clusters
+        silhouette_avg = silhouette_score(audio_array_scaled, cluster_labels)
+        scores.append(silhouette_avg)
+        print("For n_clusters =", n_clusters,
+          "The average silhouette_score is :", silhouette_avg)
+#        
+        # Compute the silhouette scores for each sample
+        sample_silhouette_values = silhouette_samples(audio_array_scaled, cluster_labels)
+        
+#        If you want to see the values for each point of the dataset
+#        print(sample_silhouette_values)
+        
+        y_lower = 10
+        
+        for i in range(n_clusters):
+            # Aggregate the silhouette scores for samples belonging to
+            # cluster i, and sort them
+            ith_cluster_silhouette_values = \
+                sample_silhouette_values[cluster_labels == i]
+#    
+            ith_cluster_silhouette_values.sort()
+#    
+            size_cluster_i = ith_cluster_silhouette_values.shape[0]
+            y_upper = y_lower + size_cluster_i
+#    
+            color = cm.nipy_spectral(float(i) / n_clusters)
+            ax1.fill_betweenx(np.arange(y_lower, y_upper),
+                              0, ith_cluster_silhouette_values,
+                              facecolor=color, edgecolor=color, alpha=0.7)
+    
+            # Label the silhouette plots with their cluster numbers at the middle
+            ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+    
+            # Compute the new y_lower for next plot
+            y_lower = y_upper + 10  # 10 for the 0 samples
+    
+            ax1.set_title("The silhouette plot for the various clusters.")
+            ax1.set_xlabel("The silhouette coefficient values")
+            ax1.set_ylabel("Cluster label")
+        
+            # The vertical line for average silhouette score of all the values
+            ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+        
+            ax1.set_yticks([])  # Clear the yaxis labels / ticks
+            ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+        
+            # 2nd Plot showing the actual clusters formed
+            colors = cm.nipy_spectral(cluster_labels.astype(float) / n_clusters)
+            ax2.scatter(audio_array_scaled[:, 0], audio_array_scaled[:, 1], marker='.', s=30, lw=0, alpha=0.7,
+                        c=colors, edgecolor='k')
+        
+            # Labeling the clusters
+            centers = clusterer.cluster_centers_
+            # Draw white circles at cluster centers
+            ax2.scatter(centers[:, 0], centers[:, 1], marker='o',
+                        c="white", alpha=1, s=200, edgecolor='k')
+        
+        for i, c in enumerate(centers):
+            ax2.scatter(c[0], c[1], marker='$%d$' % i, alpha=1,
+                        s=50, edgecolor='k')
+    
+            ax2.set_title("The visualization of the clustered data.")
+            ax2.set_xlabel("Feature space for the 1st feature")
+            ax2.set_ylabel("Feature space for the 2nd feature")
+    
+            plt.suptitle(("Silhouette analysis for KMeans clustering on sample data "
+                      "with n_clusters = %d" % n_clusters),
+                     fontsize=14, fontweight='bold')
+
+
+    plt.show()
+    
+    plt.figure()
+    plt.bar(range(n_clusters), scores, width=0.6, color="k", align="center")
+    plt.title("Silhouette scores vs number of clusters")
+        
+    
+number_of_clusters_silhouette()
+
+#%%
+
+
+
+#model. Clustering the dataset with 
+model = KMeans(n_clusters=4, init="k-means++", n_init=10, precompute_distances = True).fit(audio_array_scaled)
 labels = model.fit_predict(audio_array_scaled)
 
+
+# I LL CLEAR THIS
 #David, I don´t understand why there is so much stuff in this code ? 
 #I rewrote it in 2 lines for clarity but feel free to add anything 
 # or add why it´s there for in comments pliz
 
-#model = KMeans(n_clusters=4).fit(audio_array)
-#labels = model.fit_predict(X)
+
 #model.cluster_centers_
 #centers = np.array(model.cluster_centers_)
 #print(centers)
